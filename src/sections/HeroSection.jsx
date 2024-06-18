05a3f84559c4a7478819168d801a0e5986c43538
samuel novais <samuel.newper@gmail.com>
2024-06-17 22:38:54 -0400

    Nav-Bar att

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -1,6 +1,12 @@
+import './HeroSectionStyle.css'
 
 export const HeroSection = () => {
-    return <section className="hero-section"> 
-      <h1>Estelar</h1>
-    </section>
+    return <nav className="hero-section"> 
+      <a className='logo' href='#' > 
+          <img src="../../public/vite.svg" alt="" width={35} />
+          <img src="../../public/estelarlogo.svg" alt="" width={150}  />
+      </a>
+
+      
+    </nav>
 }
