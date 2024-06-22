492d15748f2c3f7f6da735c37eae2e6895809f44
samuel novais <samuel.newper@gmail.com>
2024-06-22 18:29:26 -0400

    text-align

--- src/components/CardMember2.jsx
+++ src/components/CardMember2.jsx
@@ -7,7 +7,7 @@ export const MemberCard2 = ({variants}) => {
     return (<motion.div variants={variants}   className="member-card">
          <div className="left-member-card2">
             <h1>Kamily Gomes - Designer</h1>
-            <p className="paragrafo-w">Responsável por criar e desenvolver interfaces visuais,  colaborar com a equipe de desenvolvimento, manter a consistência visual e garantir uma boa experiência do usuário.</p>
+            <p className="paragrafo-w2">Responsável por criar e desenvolver interfaces visuais,  colaborar com a equipe de desenvolvimento, manter a consistência visual e garantir uma boa experiência do usuário.</p>
         </div>
         
         <div className="right-member-card2">
